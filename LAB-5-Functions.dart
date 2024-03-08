666c94872a356805843391c7afa8c622f3c31a17
Mahlet Asrat <mahletasratz@gmail.com>
2024-03-08 17:26:30 +0300

    Lab-exercise

---
+++ LAB-5-Functions.dart
@@ -0,0 +1,36 @@
+abstract class Shape {
+  double calculateArea();
+}
+
+class Circle extends Shape {
+  double radius;
+
+  Circle(this.radius);
+
+  @override
+  double calculateArea() {
+    return 3.14 * radius * radius;
+  }
+}
+
+class Square extends Shape {
+  double side;
+
+  Square(this.side);
+
+  @override
+  double calculateArea() {
+    return side * side;
+  }
+}
+
+void main() {
+  Circle circle = Circle(5.0);
+  double circleArea = circle.calculateArea();
+
+  Square square = Square(4.0);
+  double squareArea = square.calculateArea();
+
+  print("Circle Area: $circleArea");
+  print("Square Area: $squareArea");
+}
