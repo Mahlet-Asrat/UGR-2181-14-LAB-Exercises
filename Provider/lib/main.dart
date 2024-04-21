3653fb9c009179862c2771a9167353e54c184e5a
Mahlet Asrat <mahletasratz@gmail.com>
2024-04-21 13:40:09 +0300

    provider counter

---
+++ Provider/lib/main.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart'; 
+import 'package:provider/provider.dart'; 
+ 
+void main() { 
+  runApp(MyApp()); 
+} 
+ 
+class Counter with ChangeNotifier { 
+  int _count = 0; 
+ 
+  int get count => _count; 
+ 
+  void increment() { 
+    _count++; 
+    notifyListeners(); 
+  } 
+ 
+  void decrement() { 
+    _count--; 
+    notifyListeners(); 
+  } 
+} 
+ 
+class MyApp extends StatelessWidget { 
+  @override 
+  Widget build(BuildContext context) { 
+    return ChangeNotifierProvider( 
+      create: (context) => Counter(), 
+      child: MaterialApp( 
+        title: 'Counter App', 
+        home: CounterScreen(), 
+      ), 
+    ); 
+  } 
+} 
+ 
+class CounterScreen extends StatelessWidget { 
+  @override 
+  Widget build(BuildContext context) { 
+    return Scaffold( 
+      appBar: AppBar( 
+        backgroundColor: const Color.fromARGB(255, 237, 224, 185),
+        title: Text('Counter App'), 
+      ), 
+      body: Center( 
+        child: Column( 
+          mainAxisAlignment: MainAxisAlignment.center, 
+          children: <Widget>[ 
+            Consumer<Counter>( 
+              builder: (context, counter, child) => Text( 
+                'Count: ${counter.count}', 
+                style: TextStyle(fontSize: 40.0, fontWeight: FontWeight.bold), 
+              ), 
+            ), 
+            SizedBox(height: 20.0), 
+            Row( 
+              mainAxisAlignment: MainAxisAlignment.center, 
+              children: <Widget>[ 
+                ElevatedButton( 
+                  onPressed: () { 
+                    Provider.of<Counter>(context, listen: false).decrement(); 
+                  }, 
+                  child: Text('Decrement'), 
+                ), 
+                SizedBox(width: 20.0), 
+                ElevatedButton( 
+                  onPressed: () { 
+                    Provider.of<Counter>(context, listen: false).increment(); 
+                  }, 
+                  child: Text('Increment'), 
+                ), 
+              ], 
+            ), 
+          ], 
+        ), 
+      ), 
+    ); 
+  } 
+}
